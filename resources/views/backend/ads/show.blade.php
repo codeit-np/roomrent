bba1823c9c49689f34fe352a20d595c5804d2dcf
Dipesh Tamang <tamangdipesh527@gmail.com>
2021-04-20 15:13:28 +0545

    dipchange

---
+++ resources/views/backend/ads/show.blade.php
@@ -0,0 +1,20 @@
+@extends('backend.app')
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                Room Information
+            </div>
+            <address>
+                location : {{ $room->location }} <br>
+                location : {{ $room-> }} <br>
+                location : {{ $room->location }} <br>
+                location : {{ $room->location }} <br>
+                location : {{ $room->location }} <br>
+            </address>
+        </div>
+    </div>
+    
+@endsection
